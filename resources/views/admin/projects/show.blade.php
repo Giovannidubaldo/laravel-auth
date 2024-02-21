7cc331a18a0ce76d522749aca33e5d519b0dabca
Giovannidubaldo <giovannidubaldo.7@gmail.com>
2024-02-21 18:52:08 +0100

    Added show

---
+++ resources/views/admin/projects/show.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.admin')
+
+@section('content')
+    <div class="container">
+        <div class="row my-5">
+            <h1>{{ $project->name }}</h1>
+            <p>{{ $project->description }}</p>
+            <p>Data di inizio: {{ $project->start_date }}</p>
+            <p>Data di fine: {{ $project->start_date }}</p>
+        </div>
+    </div>
+@endsection
