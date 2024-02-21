27517daefad13a78d14427fa3498bb5e1b305917
Giovannidubaldo <giovannidubaldo.7@gmail.com>
2024-02-21 18:34:08 +0100

    Added link for my project

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -3,8 +3,8 @@
 @section('content')
     <div class="container">
         <div class="row">
-            <div class="col-12 mt-5">
-                <a href="{{ route('admin.projects.index') }}">Vai ai miei progetti</a>
+            <div class="col-12">
+                <h2 class="text-center my-5">I miei Progetti</h2>
             </div>
         </div>
     </div>
