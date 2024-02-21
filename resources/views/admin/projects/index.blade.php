85d6fd195f4ca698f50c35aa67cb1d63e9afd124
Giovannidubaldo <giovannidubaldo.7@gmail.com>
2024-02-21 18:44:43 +0100

    Completed index.blade

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -6,6 +6,37 @@
             <div class="col-12">
                 <h2 class="text-center my-5">I miei Progetti</h2>
             </div>
+            <div class="col-12">
+                <div class="d-flex justify-content-center">
+                    <table class="table table-striped">
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>Name</th>
+                                <th>Slug</th>
+                                <th>Description</th>
+                                <th>Start Date</th>
+                                <th>End Date</th>
+                                <th>More</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($projects as $project)
+                                <tr>
+                                    <td>{{ $project->id }}</td>
+                                    <td>{{ $project->name }}</td>
+                                    <td>{{ $project->slug }}</td>
+                                    <td>{{ Str::limit($project->description, 20, '(...)') }}</td>
+                                    <td>{{ $project->start_date }}</td>
+                                    <td>{{ $project->end_date }}</td>
+                                    <td><a href="{{ route('admin.projects.show', ['project' => $project->id]) }}">more</a>
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         </div>
     </div>
 @endsection
