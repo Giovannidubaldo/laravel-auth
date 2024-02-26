ec958b18b8eaba461ac84126a8b5c892de089999
Giovannidubaldo <giovannidubaldo.7@gmail.com>
2024-02-26 15:45:42 +0100

    Update project table

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -9,5 +9,5 @@ class Project extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['name', 'start_date', 'end_date', 'slug', 'description'];
+    protected $fillable = ['name', 'start_date', 'end_date', 'slug', 'cover', 'description'];
 }
